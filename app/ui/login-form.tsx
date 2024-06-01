22a75ceb080d3219db4e7a7a59832bba03d9017f
Yonde <alisan4299@gmail.com>
2024-06-01 15:31:33 +0200

    last update

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -39,6 +39,7 @@ export default function LoginForm() {
               />
               <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
+            <p className="text-sm text-gray-900">demo: user@nextmail.com</p>
           </div>
           <div className="mt-4">
             <label
@@ -59,6 +60,7 @@ export default function LoginForm() {
               />
               <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
+            <p className="text-sm text-gray-900">demo: 123456</p>
           </div>
         </div>
         <LoginButton />
